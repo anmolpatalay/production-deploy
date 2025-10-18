5d82293f912c82065243a4df62c48aa8becc1d11
anmolpatalay <anmolpatalay@gmail.com>
2025-10-18 13:34:28 +0530

    update backend url for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [form, setForm] = useState({ name: "", age: "", gender: "" });
   const [people, setPeople] = useState([]);
 
-  const backendURL = "https://production-deploy.onrender.com"; // e.g. from Render
+  const backendURL = "https://production-deploy-1.onrender.com"; // e.g. from Render
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
